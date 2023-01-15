aaff9e9bc6897722804ea551f86263f1435f7948
greenpickles03 <a.n.royo@outlook.com>
2023-01-15 15:49:23 +0800

    Start

---
+++ src/main/java/com/xerov2/xerov2/Xerov2Application.java
@@ -0,0 +1,13 @@
+package com.xerov2.xerov2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Xerov2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Xerov2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/xerov2/xerov2/Xerov2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xerov2.xerov2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Xerov2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
